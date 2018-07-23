66b5ca6af0cfd264fc8bbab2bf23a7c5b2c237c6
Matic Drobez <matic.drobez@gmail.com>
2018-07-23 19:33:57 +0200

    GO-08: Working with variables.

---
+++ 04_variables/01_shorthand/01/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+	a := 10
+	b := "golang"
+	c := 4.42
+	d := true
+
+	fmt.Printf("%v \n", a) // %T show us which type is variable :)
+	fmt.Printf("%v \n", b)
+	fmt.Printf("%v \n", c)
+	fmt.Printf("%v \n", d)
+}
