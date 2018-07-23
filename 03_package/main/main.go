922bbde5c72f8806d7c714407c01c2d83fbe2954
Matic Drobez <matic.drobez@gmail.com>
2018-07-23 18:59:22 +0200

    GO-05: Try working with packages.

---
+++ 03_package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/wrzipxGo/03_package/stringutil"
+	"fmt"
+)
+
+func main()  {
+	fmt.Println(stringutil.reverse("OG OLLEH"))
+	fmt.Println(stringutil.name)
+}
---
+++ 03_package/stringutil/name.go
@@ -0,0 +1,4 @@
+package stringutil
+
+
+var name = "Matic"
