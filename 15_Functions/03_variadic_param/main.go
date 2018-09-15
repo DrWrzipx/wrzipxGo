0b66f0581a2d1ce2a2f8af56b04754561d0eec56
Matic Drobež <matic.drobez.extern@sixt.de>
2018-09-15 09:34:35 +0200

    Variadic functions.

--- 15_Functions/03_variadic_param/main.go
+++ 15_Functions/03_variadic_param/main.go
@@ -1,5 +1,17 @@
 package main
 
+import "fmt"
+
 func main()  {
-	
+	n := average(45, 334, 23, 94, 34)
+	fmt.Println(n)
+}
+
+func average(sf ...float64) float64 {
+	total := 0.0
+	for _, v := range sf {
+		total += v
+	}
+
+	return total / float64(len(sf))
 }
